17c85876c2b8a7207ce0b8491716cbe0abbbe1da
ajhub1 <ajhub1@student.monash.edu>
2014-07-31 16:07:06 +1000

    header fix

--- s2plot/s2plotModule.h
+++ s2plot/s2plotModule.h
@@ -14,6 +14,7 @@ namespace s2plot
 			void s2plotShutdown();
 			void initializeController();
 			int run(int argc, char** argv);
+			void draw();
 			typedef void (*callback_function)(void);
 			
 		private:
