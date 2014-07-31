58f3662f49c976677d9e16086717739736720df5
limbera <limbera@gmail.com>
2014-07-31 18:31:43 +1000

    added implementation of point, JUST STUBS no real implementation as of yet

---
+++ src/s2plotPoint.cpp
@@ -0,0 +1,17 @@
+#include "s2plot/s2plot.h"
+
+using namespace s2plot;
+using namespace omega;
+
+s2plotPoint::s2plotPoint(float x, float y, float z)
+{
+  x = x;
+  y = y;
+  z = z;
+}
+
+void s2plotPoint::renderingFunction(void (*fp)())
+{
+  
+}
+
