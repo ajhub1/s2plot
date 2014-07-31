86303b61c56603dcd7e5ed630d33443f4e5e1e16
limbera <limbera@gmail.com>
2014-07-31 18:27:09 +1000

    added header files for RenderableObject, RenderablePolyObject and RenderableCube

---
+++ s2plot/s2plotRenderableCube.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <omega.h>
+#include <omegaGl.h>
+
+namespace s2plot
+{
+  using namespace omega;
+  
+  class s2plotRenderableCube: public s2plotRenderablePolyObject
+  {
+  private:
+    float size;
+  public:
+	void draw();
+    s2plotRenderableCube(float, float, float, int, int, float);
+  };
+}
