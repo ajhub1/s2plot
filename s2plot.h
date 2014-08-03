2429e9dbebe1cccd12d33f43ab3ecd0afb6bbaa4
ajhub1 <ajhub1@student.monash.edu>
2014-08-03 23:04:26 +1000

    removed s2plotEngine.h

--- s2plot.h
+++ s2plot.h
@@ -3,6 +3,9 @@
 
 #include "s2plot/s2plot/s2plotModule.h"
 #include "s2plot/s2plot/s2plotRenderPass.h"
-#include "s2plot/s2plot/s2plotEngine.h"
+#include "s2plot/s2plot/s2plotPoint.h"
+#include "s2plot/s2plot/s2plotRenderableObject.h"
+#include "s2plot/s2plot/s2plotRenderablePolyObject.h"
+#include "s2plot/s2plot/s2plotRenderableCube.h"
 
 #endif
