915257e486e20a9c05cfbb6878170f116fbbb799
ajhub1 <ajhub1@student.monash.edu>
2014-08-03 23:12:28 +1000

    missing prototype in s2plotModule.h

--- s2plot/s2plotModule.h
+++ s2plot/s2plotModule.h
@@ -6,12 +6,13 @@ namespace s2plot
 {
 	using namespace omega;
 	
-	class s2plotModule
+	class s2plotModule: public EngineModule
 	{
 		public:
 			s2plotModule();
 			virtual ~s2plotModule();
 			void initialize();
+			void initializeRenderer(Renderer* r);
 			void update(const UpdateContext& context);	
 			void s2plotDraw();		
 	};
